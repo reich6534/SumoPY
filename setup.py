ac9f03ff9a08737a03e3fa07cb47cd96bba82b44
Rei <chiurei1@gmail.com>
2020-06-16 13:21:56 -0400

    added skeleton

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description' : 'SumoPy',
+    'author' : 'Hansoo Chang and Reiyuan Chiu',
+    'url' : 'https://github.com/Penn-ABSKids/SumoPY',
+    'download_url' : 'https://github.com/Penn-ABSKids/SumoPY/archive/master.zip',
+    'author_email' : 'hansooc@sas.upenn.edu',
+    'version' : '0.1',
+    'install_requires' : ['anaconda', 'nose'],
+    'packages' : ['SumoPy'],
+    'scripts' : [],
+    'name' : 'SumoPy'
+}
+
+setup(**config)
